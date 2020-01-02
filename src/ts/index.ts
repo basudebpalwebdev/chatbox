0a508a3e46169dfccd9a7b21a03edf87292c6c2f
Basudeb Pal <basudeb.pal.web.dev@gmail.com>
2020-01-02 18:45:40 +0530

    [2020.01.02] Almost done

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -4,8 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const chatBox = document.getElementById('chatBox');
     const chatBoxAction = chatBox!.querySelector('.chatbox-action');
     const chevronDownSvg: string = `
-        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-down"
-            class="svg-inline--fa fa-chevron-down fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg"
+        <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 448 512">
             <path fill="currentColor"
                 d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z">
@@ -13,8 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
         </svg>
     `;
     const chevronUpSvg: string = `
-        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-up" 
-            class="svg-inline--fa fa-chevron-up fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" 
+        <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" 
             viewBox="0 0 448 512">
             <path fill="currentColor" 
                 d="M240.971 130.524l194.343 194.343c9.373 9.373 9.373 24.569 0 33.941l-22.667 22.667c-9.357 9.357-24.522 9.375-33.901.04L224 227.495 69.255 381.516c-9.379 9.335-24.544 9.317-33.901-.04l-22.667-22.667c-9.373-9.373-9.373-24.569 0-33.941L207.03 130.525c9.372-9.373 24.568-9.373 33.941-.001z">
@@ -32,7 +30,10 @@ document.addEventListener('DOMContentLoaded', () => {
         chatBox!.classList.replace('minimize', 'maximize');
     }
 
-    setTimeout(() => chatBox!.classList.replace('hide', 'show'), 5000);
+    setTimeout(() => {
+        chatBox!.classList.remove('hidden');
+        isMinimized ? chatBox!.classList.add('minimize') : chatBox!.classList.add('maximize');
+    }, 5000);
 
     chatBoxAction!.addEventListener('click', () => {
         if (isMinimized) {
